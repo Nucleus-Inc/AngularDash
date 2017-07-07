28f1c03e22b3096e333834d5388dd6160142f15d
Igor Lopes <igor@nucleus.eti.br>
2017-07-07 01:06:53 -0300

    Update forms view

--- app/controllers/FormsController.js
+++ app/controllers/FormsController.js
@@ -1,5 +1,16 @@
 (function() {
     angular.module('Dashboard').controller('FormsController', ['$scope', function($scope) {
 
+        var vm = this;
+
+        this.inputs = [];
+
+        $scope.test = function() {
+            if ($scope.formModal.$valid) {
+                console.log("valid");
+            } else {
+                console.log("not valid");
+            }
+        }
     }]);
 }());
