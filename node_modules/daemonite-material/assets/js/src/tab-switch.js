7836dd95463237ebf1779b27266d9a3bbb0fc8ce
Augusto Neto <augusto@nucleus.eti.br>
2017-07-21 23:04:08 -0300

    git commit -m

---
+++ node_modules/daemonite-material/assets/js/src/tab-switch.js
@@ -0,0 +1,133 @@
+/*!
+ * tab indicator animation
+ * requires bootstrap's (v4.0.0-alpha.6) tab.js
+ */
+const TabSwitch = (($) => {
+  // constants >>>
+    const DATA_KEY            = 'md.tabswitch';
+    const NAME                = 'tabswitch';
+    const NO_CONFLICT         = $.fn[NAME];
+    const TRANSITION_DURATION = 300;
+
+    const ClassName = {
+      ANIMATE    : 'animate',
+      INDICATOR  : 'nav-tabs-indicator',
+      MATERIAL   : 'nav-tabs-material',
+      SCROLLABLE : 'nav-tabs-scrollable',
+      SHOW       : 'show'
+    };
+
+    const Event = {
+      SHOW_BS_TAB : 'show.bs.tab'
+    };
+
+    const Selector = {
+      DATA_TOGGLE : '.nav-tabs [data-toggle="tab"]',
+      NAV         : '.nav-tabs',
+      NAV_ITEM    : '.nav-item'
+    };
+  // <<< constants
+
+  class TabSwitch {
+    constructor(nav) {
+      if (typeof $.fn.tab === 'undefined') {
+        throw new Error('Material\'s JavaScript requires Bootstrap\'s tab.js');
+      };
+
+      this._nav          = nav;
+      this._navindicator = null;
+    }
+
+    switch(element, relatedTarget) {
+      let supportsTransition = Util.supportsTransitionEnd();
+
+      if (!this._navindicator) {
+        this._createIndicator();
+      }
+
+      let elLeft        = $(element).closest(Selector.NAV_ITEM).offset().left;
+      let elWidth       = $(element).closest(Selector.NAV_ITEM).outerWidth();
+      let navLeft       = $(this._nav).offset().left;
+      let navScrollLeft = $(this._nav).scrollLeft();
+      let navWidth      = $(this._nav).outerWidth();
+
+      if (relatedTarget !== undefined) {
+        let relatedLeft  = $(relatedTarget).closest(Selector.NAV_ITEM).offset().left;
+        let relatedWidth = $(relatedTarget).closest(Selector.NAV_ITEM).outerWidth();
+
+        $(this._navindicator).css({
+          left  : ((relatedLeft + navScrollLeft) - navLeft),
+          right : (navWidth - ((relatedLeft + navScrollLeft) - navLeft + relatedWidth))
+        });
+
+        $(this._navindicator).addClass(ClassName.SHOW);
+        Util.reflow(this._navindicator);
+
+        if (supportsTransition) {
+          $(this._nav).addClass(ClassName.ANIMATE);
+        }
+      }
+
+      $(this._navindicator).css({
+        left  : ((elLeft + navScrollLeft) - navLeft),
+        right : (navWidth - ((elLeft + navScrollLeft) - navLeft + elWidth))
+      });
+
+      let complete = () => {
+        $(this._nav).removeClass(ClassName.ANIMATE);
+        $(this._navindicator).removeClass(ClassName.SHOW);
+      }
+
+      if (!supportsTransition) {
+        complete();
+        return;
+      }
+
+      $(this._navindicator)
+        .one(Util.TRANSITION_END, complete)
+        .emulateTransitionEnd(TRANSITION_DURATION);
+    }
+
+    _createIndicator() {
+      this._navindicator = document.createElement('div');
+
+      $(this._navindicator)
+        .addClass(ClassName.INDICATOR)
+        .appendTo(this._nav);
+
+      $(this._nav).addClass(ClassName.MATERIAL);
+    }
+
+    static _jQueryInterface(relatedTarget) {
+      return this.each(function () {
+        let nav = $(this).closest(Selector.NAV)[0];
+
+        if (!nav) {
+          return;
+        }
+
+        let data = $(nav).data(DATA_KEY);
+
+        if (!data) {
+          data = new TabSwitch(nav);
+          $(nav).data(DATA_KEY, data);
+        }
+
+        data.switch(this, relatedTarget);
+      });
+    }
+  }
+
+  $(document).on(Event.SHOW_BS_TAB, Selector.DATA_TOGGLE, function (event) {
+    TabSwitch._jQueryInterface.call($(event.target), event.relatedTarget);
+  });
+
+  $.fn[NAME]             = TabSwitch._jQueryInterface;
+  $.fn[NAME].Constructor = TabSwitch;
+  $.fn[NAME].noConflict  = function () {
+    $.fn[NAME] = NO_CONFLICT;
+    return TabSwitch._jQueryInterface;
+  };
+
+  return TabSwitch;
+})(jQuery);
