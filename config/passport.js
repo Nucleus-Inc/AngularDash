1e2224584ce80a4208e0a03867dcd17458aaf4e1
Eduardo Castro <eduardo@nucleus.eti.br>
2017-11-20 15:16:54 -0200

    readme added

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,5 @@ var mongoose = require('mongoose')
 
 module.exports = function () {
   var Admin = mongoose.model('Admin')
-  var Seller = mongoose.model('Seller')
-
   require('./strategies/admin-local.js')(Admin)
-  require('./strategies/seller-local.js')(Seller)
 }
